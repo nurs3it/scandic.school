f5cfe222127ab8e1859e36c812bcc916ff065829
Nurseiit Sharip <nurseit.js@gmail.com>
2025-10-21 14:12:42 +0500

    Add new remote image pattern for admin subdomain in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'admin.scandicschools.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
         hostname: 'images.unsplash.com',
         port: '',
         pathname: '/**',
